89fbe5c26becd98e01be7878b3dc0dc360245cb0
William Freeman <wfreeman@fmgl.com.au>
2022-02-14 13:33:46 +0800

    refined bot theory

--- bot_logic.py
+++ bot_logic.py
@@ -11,8 +11,8 @@ class dice_params:
         self.dice_no = dice_no
     
 
-pre_insults = ['moronic', 'obtuse', 'inane', 'rotund', 'fat', 'surly', 'ignorant', 'charged', 'addicted', 'overt', 'snobbish', 'irrepressible']
-post_insults = ['slattern', 'grox fucker', 'turkey', 'whoreson', 'fat cat', 'brigand', 'illiterate', 'cunt', 'whore']
+insult_predicate = ['moronic', 'obtuse', 'inane', 'rotund', 'fat', 'surly', 'ignorant', 'charged', 'addicted', 'overt', 'snobbish', 'irrepressible']
+insults = ['slattern', 'grox fucker', 'turkey', 'whoreson', 'fat cat', 'brigand', 'illiterate', 'cunt', 'whore']
 
 def roll_dice(params):
     i = 1
@@ -24,7 +24,7 @@ def roll_dice(params):
     #handle case of too many rolls for discrete
     if params.is_distinct:
         if(params.dice_no > params.dice_size):
-            return f"oi {get_insult('short form')}, too many dice rolled versus dice options"
+            return f"oi {get_insult('short form')}, too many dice rolled for the amount of possible distinct options"
     #roll dice, handle discrete and addition case as well
     while i <= params.dice_no:
         roll = random.randint(1, params.dice_size) 
@@ -37,7 +37,7 @@ def roll_dice(params):
     rolls.sort()
     return rolls
                     
-async def format_dice_args(ctx, args):  
+async def format_dice_params(ctx, args):  
     global addition_arg
     global is_distinct
     global dice_size
@@ -53,7 +53,7 @@ async def format_dice_args(ctx, args):
         dice_no = int(dice_args[0] or 1)
         dice_size = int(dice_args[1])     
     else: 
-        await ctx.reply(f"give me a good format you {get_insult('long form')}, something like !roll 1d6 or !roll d6")
+        await ctx.reply(f"give me a good format you {get_insult('long form')}, something like !roll 1d6 or !roll d6, max 2 digits so 1-99")
         raise error("wrong dice format used")
 
     if len(args) > 1:
@@ -70,8 +70,8 @@ async def format_dice_args(ctx, args):
 
 def get_insult(arg):
     if arg == "long form":
-        insult = f"{pre_insults[random.randint(0, len(pre_insults)-1)]} {post_insults[random.randint(0, len(post_insults)-1)]}"
+        insult = f"{insult_predicate[random.randint(0, len(insult_predicate)-1)]} {insults[random.randint(0, len(insults)-1)]}"
     else:
-        insult = f"{post_insults[random.randint(0, len(post_insults)-1)]}"
+        insult = f"{insults[random.randint(0, len(insults)-1)]}"
     return insult
 
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import discord
 from discord.ext import commands
 import os
-from bot_logic import dice_params, format_dice_args, roll_dice, get_insult
+from bot_logic import format_dice_params, roll_dice, get_insult
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", case_insensitive=True)
-
 
 @bot.event
 async def on_ready():
@@ -16,12 +15,11 @@ async def on_ready():
 
 @bot.command(name="roll", brief="Unable to do basic mafs? ole Reggie knows his letters", help="ole Reggie will roll some dice, accepted formats: XXdXX ie 1d4 or 10d20, also accepts parameters after like 'distinct' to keep rolls distinct. eg. !roll 10d4 distinct",usage="[0-2]<number> d [1-2]<number> eg. 10d20 or d4, optional parameters: '+number' to add to rolls and 'distinct' to keep only unique rolls")
 async def roll(ctx, *args):
-
     try: args
     except: 
         await ctx.send(f"you gotta specify some dice arguments you {get_insult('long form')}, try 1d6 or something")
         return
-    params = await format_dice_args(ctx, args)
+    params = await format_dice_params(ctx, args)
     result = roll_dice(params)
     if isinstance(result, list):
         await ctx.reply(f"Ere's your bloody dice: \r\n \r\n{result}\r\n\r\nYou {get_insult('long form')}")
