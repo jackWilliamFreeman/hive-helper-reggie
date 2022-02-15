8aab717e5e32c83bcecd49e08892b706eaa2272a
William Freeman <wfreeman@fmgl.com.au>
2022-02-15 16:05:50 +0800

    changes to logic

--- bot_logic.py
+++ bot_logic.py
@@ -55,20 +55,56 @@ def get_scenario(user1, user2, is_raider):
     
     badlands_scenario = get_badlands_scenario()
     scenario = get_main_scenario_details(is_raider)
-    global gang_size_text 
-    gang_size_text = ""
-    if scenario.gang_size == "Choose d3+6 Gangers":
-        user1_calc_gang_size = random.randint(1,3)+6
-        user2_calc_gang_size = random.randint(1,3)+6
-        gang_size_text = f"congrats {user1} you get: **{user1_calc_gang_size} gangers**, congrats {user2} you get **{user2_calc_gang_size} gangers**\r\n"
+    gang_size_text = get_gang_size_text(user1, user2, scenario)
+    equipment_text = ''
+
+    if scenario.scenario_name == "Big Score":
+        house_equip_text, standard_equip_text, settlement_defence_text = get_trap_text()
+        equipment_text = f'You also get to deal with these babies: \r\n\
+            **{house_equip_text}**\r\n\
+            **{standard_equip_text}**\r\n\
+            **{settlement_defence_text}**\r\n'
 
     return_text = f"YO LISTEN UP: we got ourselves a battle now between {user1} and {user2}\r\
     Now i always thought {user1} was a {get_insult('short form')} and {user2} was a {get_insult('short form')} but we got to lay some ground rules \r\
     The scenario do be: **{scenario.scenario_name}**... {scenario.comment} \r\
     The gang rules are: **{scenario.gang_size}**\r\
     {gang_size_text}\
+    {equipment_text}\
     Now we be rolling in the badlands so make sure to beware of the event: **{badlands_scenario}**"
     return return_text
+
+def get_trap_text():
+    items = pd.read_csv('traps_gear.csv', header=0)
+    house_equip_text = get_house_equipment(items)
+    standard_equip_text = get_standard_equipment(items, 'Standard traps')
+    settlement_defence_text = get_standard_equipment(items, 'Settlement defence')
+    return house_equip_text,standard_equip_text,settlement_defence_text
+
+def get_standard_equipment(items, arg):
+    dice_roll = random.randint(1,6)
+    house_gear_list = items.loc[(items['dice_roll']==dice_roll) & (items['theme']==arg)]
+    text = f"{house_gear_list.description.values.tolist()[0]}"
+    return text
+    
+def get_house_equipment(items):
+    dice_roll = random.randint(1,8)
+    house_gear_list = items.loc[(items['dice_roll']==dice_roll) & (items['theme']=='house trap')]
+    if house_gear_list.roll_dice.values.tolist()[0] == 1:
+        dice_size = house_gear_list.param.values.tolist()[0]
+        roll = random.randint(1, dice_size)
+        text = f"{roll} X {house_gear_list.description.values.tolist()[0]}"
+    else:
+        text = f"{house_gear_list.param.values.tolist()[0]} X {house_gear_list.description.values.tolist()[0]}"
+    return text
+
+def get_gang_size_text(user1, user2, scenario):
+    gang_size_text = ""
+    if scenario.gang_size == "Choose d3+6 Gangers":
+        user1_calc_gang_size = random.randint(1,3)+6
+        user2_calc_gang_size = random.randint(1,3)+6
+        gang_size_text = f"congrats {user1} you get: **{user1_calc_gang_size} gangers**, congrats {user2} you get **{user2_calc_gang_size} gangers**\r\n"
+    return gang_size_text
 
 
 def get_main_scenario_details(is_raider):
